d2e7d62c72033d739448b1b2ec7aacb4e52b8bcd
Saidabrorkhon Shavkatbekov <97293714+saidabroor@users.noreply.github.com>
2025-04-14 17:04:12 +0900

    ok

--- 3rd-month/4th-lesson/deployment.py
+++ 3rd-month/4th-lesson/deployment.py
@@ -4,3 +4,27 @@ from joblib import load
 from sklearn.metrics import accuracy_score
 
 model = load('song_popularity.joblib')
+
+try:
+  x_test=pd.read_csv('song_x_test.csv')
+  y_test=pd.read_csv('song_y_test.csv')
+  y_test=y_test.squeeze()
+  show_accuracy=True
+except:
+  show_accuracy=False
+
+st.set_page_config(page_title='Song popularity prediction model', layout='centered')
+st.title('Song popularity')
+st.markdown('Enter charactersitcs of the song and see how is the popularity of that song using our model.')
+
+song_name = st.text_input('Song name')
+song_duration_ms = st.number_input('Song duration', min_value=0.0, max_value=1000.0, value=300.0)
+acousticness = st.number_input('Acousticness level', min_value=0.0, max_value=100.0)
+danceability = st.number_input('Danceability level', min_value=0.0, max_value=100.0)
+energy = st.number_input('Energy level', min_value=0.0, max_value=100.0)
+instrumentalness = st.number_input('Instrumentalness level', min_value=0.0, max_value=100.0)
+liveness = st.number_input('Liveness level', min_value=0.0, max_value=100.0)
+loudness = st.number_input('Loudness level', min_value=00.0, max_value=60.0)
+speechiness = st.number_input('Speechiness level', min_value=0.0, max_value=100.0)
+tempo = st.number_input('Tempo level', min_value=50.0, max_value=200.0)
+audio_valence = st.number_input('Audio_valence level', min_value=0.0, max_value=100.0)
